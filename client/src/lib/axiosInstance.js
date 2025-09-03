3621343534e007bc058774e666d31bd19bd8cefc
sahilsinghchouhan <161635928+sahilsinghchouhan@users.noreply.github.com>
2025-09-04 01:23:05 +0530

    fixed url

--- client/src/lib/axiosInstance.js
+++ client/src/lib/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://chatmate-op5l.onrender.com";
+const BASE_URL = "https://chatmate-op5l.onrender.com/api/v1";
 
 const axiosInstance = axios.create();
 
